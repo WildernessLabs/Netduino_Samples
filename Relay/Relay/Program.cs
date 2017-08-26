8820e7deed8caf6b97ea2855df469dd0282c254b
bryan costanich <b@wowzer.net>
2017-08-25 17:01:20 -0700

    Added relay sample.

---
+++ Relay/Relay/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+using System.Threading;
+
+namespace Relay
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			// create an output port (a port that can be written to) and connect it to Digital Pin 2
+            OutputPort relay = new OutputPort(Pins.GPIO_PIN_D2, false);
+
+			// run forever
+			while (true)
+			{
+                relay.Write(true); // turn on the LED
+				Thread.Sleep(500); // sleep for 1/2 second
+				relay.Write(false); // turn off the relay
+				Thread.Sleep(500); // sleep for 1/2 second
+			}
+		}
+	}
+}
