3291b63ff1352e090f099b3d67f4c6054d74b0e0
bryan costanich <b@wowzer.net>
2017-08-25 20:42:57 -0700

    converted tabs to spaces and added supporting images, etc.

--- Relay/Relay/Program.cs
+++ Relay/Relay/Program.cs
@@ -6,21 +6,21 @@ using System.Threading;
 
 namespace Relay
 {
-	public class Program
-	{
-		public static void Main()
-		{
-			// create an output port (a port that can be written to) and connect it to Digital Pin 2
+    public class Program
+    {
+        public static void Main()
+        {
+            // create an output port (a port that can be written to) and connect it to Digital Pin 2
             OutputPort relay = new OutputPort(Pins.GPIO_PIN_D2, false);
 
-			// run forever
-			while (true)
-			{
+            // run forever
+            while (true)
+            {
                 relay.Write(true); // turn on the LED
-				Thread.Sleep(500); // sleep for 1/2 second
-				relay.Write(false); // turn off the relay
-				Thread.Sleep(500); // sleep for 1/2 second
-			}
-		}
-	}
+                Thread.Sleep(500); // sleep for 1/2 second
+                relay.Write(false); // turn off the relay
+                Thread.Sleep(500); // sleep for 1/2 second
+            }
+        }
+    }
 }
