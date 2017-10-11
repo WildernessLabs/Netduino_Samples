610dd43f08e585e4885e8709c5676e6b6ccd0b7a
bryan costanich <b@wowzer.net>
2017-10-10 19:32:49 -0700

    Added Connected Coffee Maker simple control example.

---
+++ Connected_CoffeeMaker/Connected_Coffee_SimpleControl/Connected_Coffee_SimpleControl/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+using System.Threading;
+
+namespace Connected_Coffee_SimpleControl
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			// create an output port (a port that can be written to) and connect it to Digital Pin 2
+			OutputPort relay = new OutputPort(Pins.GPIO_PIN_D2, false);
+
+			// run forever
+			while (true)
+			{
+				relay.Write(true); // turn on the relay
+				Thread.Sleep(5000); // Leave on for 5 seconds
+				relay.Write(false); // turn off the relay
+				Thread.Sleep(5000); // leave off for 5 seconds
+			}
+		}
+	}
+}
