3e931fe0d755e208e1ed6a15562d6e44b66db95c
bryan costanich <b@wowzer.net>
2018-05-10 12:32:37 -0700

    UI re-work, and update to new n.foundation code.

--- Netduino.Foundation/FoodDehydrator3000/FoodDehydrator3000/App.cs
+++ Netduino.Foundation/FoodDehydrator3000/FoodDehydrator3000/App.cs
@@ -1,6 +1,7 @@
 using FoodDehydrator3000.Properties;
 using Maple;
 using Microsoft.SPOT;
+using H = Microsoft.SPOT.Hardware;
 using Microsoft.SPOT.Net.NetworkInformation;
 using Netduino.Foundation.Displays;
 using Netduino.Foundation.Displays.LCD;
@@ -14,16 +15,19 @@ using Netduino.Foundation.ICs.IOExpanders.MCP23008;
 using System;
 using System.Collections;
 using N = SecretLabs.NETMF.Hardware.Netduino;
+using Netduino.Foundation;
+using Netduino.Foundation.Sensors.Buttons;
 
 namespace FoodDehydrator3000
 {
     public class App
     {
         // peripherals
+        protected PushButton _pushButton = null;
         protected AnalogTemperature _tempSensor = null;
         protected SoftPwm _heaterRelayPwm = null;
         protected Relay _fanRelay = null;
-        //protected PushButton _button = null;
+        protected PushButton _button = null;
         protected ITextDisplay _display = null;
 
         RotaryEncoderWithButton _encoder = null;
@@ -34,18 +38,46 @@ namespace FoodDehydrator3000
         // vars
         protected NetworkInterface[] _interfaces;
         protected float _currentTemp;
-        MapleServer server;
-        Menu _menu;
-        private DateTime _tempUpdated;
-        private float _targetTemp;
-        private TimeSpan _runTime;
+        protected MapleServer _server;
+        protected Menu _menu;
+        protected DateTime _tempUpdated;
+        protected float _targetTemp;
+        protected TimeSpan _runTime;
+        protected bool _inMenu = false;
 
         public App()
         {
+            // initialze
+            this.InitializePeripherals();
+            this.InitializeMenu();
+
+            // initilaize our dehydrator controller
+            _dehydrator = new DehydratorController(_tempSensor, _heaterRelayPwm, _fanRelay, _display);
+
+            // show our info screen
+            this.DisplayInfoScreen();
+
+            // 
+            this.WireUpPeripheralEvents();
+
+            // setup our web server
+            this.InitializeWebServer();
+        }
+
+        /// <summary>
+        /// Configures the hardware perihperals (LCD, temp sensor, relays, etc.) 
+        /// so they can be used by the application.
+        /// </summary>
+        protected void InitializePeripherals()
+        {
+            // pushbutton (for testing)
+            _pushButton = new PushButton(
+                (H.Cpu.Pin)0x15, CircuitTerminationType.Floating);
+
             // Rotary Encoder
             _encoder = new RotaryEncoderWithButton(
                 N.Pins.GPIO_PIN_D7, N.Pins.GPIO_PIN_D6, N.Pins.GPIO_PIN_D5,
-                Netduino.Foundation.CircuitTerminationType.CommonGround);
+                CircuitTerminationType.CommonGround);
 
             // LCD
             //_display = new Lcd2004(new MCP23008());
@@ -55,53 +87,117 @@ namespace FoodDehydrator3000
             _display.WriteLine("Display up!", 0);
 
             // Analog Temp Sensor. Setup to notify at half a degree changes
-            _tempSensor = new AnalogTemperature(N.AnalogChannels.ANALOG_PIN_A3,
+            _tempSensor = new AnalogTemperature(N.AnalogChannels.ANALOG_PIN_A0,
                 AnalogTemperature.KnownSensorType.LM35, temperatureChangeNotificationThreshold: 0.5F);
-            _tempSensor.TemperatureChanged += (object sender, SensorFloatEventArgs e) => {
-                UpdateTemp(e.CurrentValue);
-            };
-            // display our initial temp
-            UpdateTemp(_tempSensor.Temperature);
             Debug.Print("TempSensor up.");
             _display.WriteLine("Temp Sensor up!", 1);
 
             // Heater driven by Software PWM
-            _heaterRelayPwm = new SoftPwm(N.Pins.GPIO_PIN_D3, 0.5f, 1.0f / 30.0f);
+            _heaterRelayPwm = new SoftPwm(N.Pins.GPIO_PIN_D2, 0.5f, 1.0f / 30.0f);
             Debug.Print("Heater PWM up.");
-            _display.WriteLine("Heater PWM up!", 0);
+            _display.WriteLine("Heater PWM up!", 2);
 
             // Fan Relay
-            _fanRelay = new Relay(N.Pins.GPIO_PIN_D2);
+            _fanRelay = new Relay(N.Pins.GPIO_PIN_D3);
             Debug.Print("Fan up.");
-            _display.WriteLine("Fan up!", 1);
+            _display.WriteLine("Fan up!", 3);
 
+            // output status
             Debug.Print("Peripherals up");
-            _display.WriteLine("All systems up!", 1);
+            _display.WriteLine("Peripherals online!", 0);
+        }
 
-            _menu = new Menu(_display, _encoder, Resources.GetBytes(Resources.BinaryResources.menu));
+        //
+        protected void WireUpPeripheralEvents()
+        {
+            // Analog Temp Sensor. Setup to notify at half a degree changes
+            _tempSensor.TemperatureChanged += (object sender, SensorFloatEventArgs e) => {
+                UpdateTemp(e.CurrentValue);
+            };
+
+            _pushButton.LongPressClicked += (s, e) => {
+                Debug.Print("Long press.");
+                _targetTemp = 40;
+                TogglePower();
+            };
+
+            _encoder.Clicked += (s, e) =>
+            {
+                // if the menu isn't displayed, display it. otherwise
+                // encoder click events are handled by menu
+                if (!_inMenu) {
+                    this.DisplayMenu();
+                }
+            };
+        }
+
+        protected void InitializeMenu()
+        {
+            // initialize menu
+            _menu = new Menu(_display, _encoder, _encoder.Button, Resources.GetBytes(Resources.BinaryResources.menu));
             _menu.ValueChanged += HandleMenuValueChange;
             _menu.Selected += HandleMenuSelected;
+        }
 
-            _dehydrator = new DehydratorController(_tempSensor, _heaterRelayPwm, _fanRelay, _display);
-
+        protected void InitializeWebServer()
+        {
+            // configure our web server
             RequestHandler handler = new RequestHandler();
             handler.TurnOff += Handler_TurnOff;
             handler.TurnOn += Handler_TurnOn;
             handler.GetStatus += Handler_GetStatus;
 
-            server = new MapleServer();
-            server.AddHandler(handler);
+            _server = new MapleServer();
+            _server.AddHandler(handler);
+
         }
 
-        private void HandleMenuSelected(object sender, MenuSelectedEventArgs e)
+        /// <summary>
+        /// Closes the menu (if open), and displays the info screen which 
+        /// has temp and such on it.
+        /// </summary>
+        protected void DisplayInfoScreen()
+        {
+            if(_inMenu) CloseMenu();
+            
+            //TEMP
+            _display.Clear();
+
+            _display.WriteLine("Current Temp: " + _tempSensor.Temperature.ToString("F1") + "C", 0);
+            _display.WriteLine("Target:" + _targetTemp.ToString("F0") + "C", 1);
+            var remainingTime = _dehydrator.RunningTimeLeft;
+            _display.WriteLine("Time: " + PadLeft(remainingTime.Hours.ToString(), '0', 2) + ":" + PadLeft(remainingTime.Minutes.ToString(), '0', 2), 2);
+            _display.WriteLine("Click for more.", 3);
+        }
+
+        protected void DisplayMenu()
+        {
+            this._inMenu = true;
+            this._menu.Enable();
+        }
+
+        protected void CloseMenu()
+        {
+            this._menu.Disable();
+            this._inMenu = false;
+        }
+
+        /// <summary>
+        /// Called when an item in the menu is selected.
+        /// </summary>
+        protected void HandleMenuSelected(object sender, MenuSelectedEventArgs e)
         {
             if(e.Command == "power")
             {
+                Debug.Print("menu power");
                 TogglePower();
             }
         }
 
-        private void HandleMenuValueChange(object sender, ValueChangedEventArgs e)
+        /// <summary>
+        /// Called when an item in the menu changes.
+        /// </summary>
+        protected void HandleMenuValueChange(object sender, ValueChangedEventArgs e)
         {
             if(e.ItemID == "temperature")
             {
@@ -115,25 +211,35 @@ namespace FoodDehydrator3000
             }
         }
 
+        /// <summary>
+        /// Called when the temperature data has changed. 
+        /// Updates the display and menu data.
+        /// </summary>
+        /// <param name="temp"></param>
         protected void UpdateTemp(float temp)
         {
             int updateInterval = 5;
 
-            if(_menu != null)
+            if (!_inMenu)
             {
-                if(DateTime.Now > _tempUpdated.AddSeconds(updateInterval))
-                {
-                    Debug.Print("Update display");
-                    TimeSpan remainingTime = _dehydrator.RunningTimeLeft;
-
-                    Hashtable values = new Hashtable();
-                    values.Add("displayCurrentTemp", temp);
-                    values.Add("temperature", _targetTemp);
-                    values.Add("displayRemainingTime", PadLeft(remainingTime.Hours.ToString(), '0', 2) + ":" + PadLeft(remainingTime.Minutes.ToString(), '0', 2));
-                    _menu.UpdateItemValue(values);
-                    _tempUpdated = DateTime.Now;
-                }
+                DisplayInfoScreen();
             }
+
+            //if(_menu != null)
+            //{
+            //    if(DateTime.Now > _tempUpdated.AddSeconds(updateInterval))
+            //    {
+            //        Debug.Print("Update display");
+            //        TimeSpan remainingTime = _dehydrator.RunningTimeLeft;
+
+            //        Hashtable values = new Hashtable();
+            //        values.Add("displayCurrentTemp", temp);
+            //        values.Add("temperature", _targetTemp);
+            //        values.Add("displayRemainingTime", PadLeft(remainingTime.Hours.ToString(), '0', 2) + ":" + PadLeft(remainingTime.Minutes.ToString(), '0', 2));
+            //        _menu.UpdateItemValue(values);
+            //        _tempUpdated = DateTime.Now;
+            //    }
+            //}
         }
 
         protected void TogglePower()
@@ -156,14 +262,14 @@ namespace FoodDehydrator3000
 
             if (networkInit)
             {
-                server.Start();
+                _server.Start();
                 Debug.Print("Maple server started.");
             }
         }
 
         public void Stop()
         {
-            server.Stop();
+            _server.Stop();
             Debug.Print("Maple server stopped.");
         }
 
