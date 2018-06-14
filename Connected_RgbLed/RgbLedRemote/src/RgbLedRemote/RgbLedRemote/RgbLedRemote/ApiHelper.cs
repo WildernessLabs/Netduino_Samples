be37b09caa2b4eb1e39bfc4e12bae7381c58d7bf
Jorge Ramirez <ramirez.jorgea@outlook.com>
2018-06-13 19:39:37 -0700

    Revert "Rgb Maple Update"

---
+++ Connected_RgbLed/RgbLedRemote/src/RgbLedRemote/RgbLedRemote/RgbLedRemote/ApiHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace RgbLedRemote
+{
+    public class ApiHelper
+    {
+        public ApiHelper() { }
+
+        public async Task<bool> SendCommand(string command)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://" + App.HostAddress + "/");
+            client.Timeout = TimeSpan.FromSeconds(5);
+
+            try
+            {
+                var response = await client.PostAsync(command, null);
+                return response.IsSuccessStatusCode;
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.Print(ex.Message);
+                return false;
+            }
+        }
+    }
+}
