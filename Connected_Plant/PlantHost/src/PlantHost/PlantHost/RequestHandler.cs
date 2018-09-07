098728b685ab00d77aeca7d5661c48af71ca390d
Jorge Ramirez <ramirez.jorgea@outlook.com>
2018-09-06 21:51:10 -0700

    [PlantHost] Calibrated soil values

--- Connected_Plant/PlantHost/src/PlantHost/PlantHost/RequestHandler.cs
+++ Connected_Plant/PlantHost/src/PlantHost/PlantHost/RequestHandler.cs
@@ -19,15 +19,17 @@ namespace PlantHost
             var d7 = new Microsoft.SPOT.Hardware.OutputPort(Pins.GPIO_PIN_D7, false);
 
             int sample = 0;
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 3; i++)
             {
                 d7.Write(true);
                 Thread.Sleep(5);
                 sample += a0.Read();
                 d7.Write(false);
+
+                Thread.Sleep(500);
             }
 
-            humidity = 100 - Map((sample / 5), 400, 1023, 0, 100);
+            humidity = 100 - Map((sample / 3), 250, 1023, 0, 100);
             Debug.Print("humidity =" + humidity);
             StatusResponse();
         }
