6e984ce8f734356934d24df3f7fcc450c2e00a47
Jorge Ramirez <ramirez.jorgea@outlook.com>
2018-09-16 18:39:13 -0700

    Hackster project sample

---
+++ Blinky_MCP23008/src/Blinky_MCP23008/Blinky_MCP23008/App.cs
@@ -0,0 +1,49 @@
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using Netduino.Foundation.ICs.IOExpanders.MCP23008;
+using SecretLabs.NETMF.Hardware.Netduino;
+using System.Threading;
+
+namespace Blinky_MCP23008
+{
+    public class App
+    {
+        static MCP23008 _mcp = null;
+
+        public App()
+        {
+            InitializePeripherals();
+        }
+
+        protected void InitializePeripherals()
+        {
+            _mcp = new MCP23008(39);
+        }
+
+        public void Run()
+        {
+            // create an array of ports
+            DigitalOutputPort[] ports = new DigitalOutputPort[8];
+            for (byte i = 0; i <= 7; i++)
+            {
+                ports[i] = _mcp.CreateOutputPort(i, false);
+            }
+
+            while (true)
+            {
+                // count from 0 to 7 (8 leds)
+                for (int i = 0; i <= 7; i++)
+                {
+                    // turn on the LED that matches the count
+                    for (byte j = 0; j <= 7; j++)
+                    {
+                        ports[j].State = (i == j);
+                    }
+
+                    Debug.Print("i: " + i.ToString());
+                    Thread.Sleep(250);
+                }
+            }
+        }
+    }
+}
