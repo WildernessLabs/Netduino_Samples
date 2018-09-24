fc396a5843ed7044fdbcff859400d62bde1b8bee
Jorge Ramirez <ramirez.jorgea@outlook.com>
2018-09-23 23:48:05 -0700

    [PlantHost] Refactoring App.cs to add HumiditySoilController and schedule sensing times using RTC

--- Connected_Plant/PlantHost/src/PlantHost/PlantHost/App.cs
+++ Connected_Plant/PlantHost/src/PlantHost/PlantHost/App.cs
@@ -2,14 +2,20 @@
 using Microsoft.SPOT;
 using Netduino.Foundation.LEDs;
 using Netduino.Foundation.Network;
-using SecretLabs.NETMF.Hardware.Netduino;
+using Netduino.Foundation.RTCs;
+using SLH = SecretLabs.NETMF.Hardware;
+using N = SecretLabs.NETMF.Hardware.Netduino;
+using MSH = Microsoft.SPOT.Hardware;
+using System;
 
 namespace PlantHost
 {
     public class App
     {
-        RgbPwmLed rgbPwmLed;
+        protected DS3231 _rtc;
+        protected RgbPwmLed _rgbPwmLed;
         protected MapleServer _server;
+        protected HumiditySensorController _humiditySensor;
 
         public App()
         {
@@ -19,18 +25,26 @@ namespace PlantHost
 
         protected void InitializePeripherals()
         {
-            rgbPwmLed = new RgbPwmLed
+            _rtc = new DS3231(0x68, 100);
+
+            _rgbPwmLed = new RgbPwmLed
             (
-                PWMChannels.PWM_PIN_D6,
-                PWMChannels.PWM_PIN_D5,
-                PWMChannels.PWM_PIN_D3,
+                N.PWMChannels.PWM_PIN_D6,
+                N.PWMChannels.PWM_PIN_D5,
+                N.PWMChannels.PWM_PIN_D3,
                 1.05f,
                 1.5f,
                 1.5f,
                 false
             );
 
-            rgbPwmLed.SetColor(Netduino.Foundation.Color.Red);
+            _humiditySensor = new HumiditySensorController
+            (
+                N.Pins.GPIO_PIN_A0,
+                N.Pins.GPIO_PIN_D7
+            );
+
+            _rgbPwmLed.SetColor(Netduino.Foundation.Color.Red);
         }
 
         protected void InitializeWebServer()
@@ -50,7 +64,10 @@ namespace PlantHost
         void InitializerNetworkConnected(object sender, EventArgs e)
         {
             _server.Start("PlantHost", Initializer.CurrentNetworkInterface.IPAddress);
-            rgbPwmLed.SetColor(Netduino.Foundation.Color.Green);
+            _rgbPwmLed.SetColor(Netduino.Foundation.Color.Green);
+
+            Debug.Print("Date now - " + _rtc.CurrentDateTime);
+            Debug.Print("Soil humidity - " + _humiditySensor.Read());
         }
     }
 }
--- Connected_Plant/PlantHost/src/PlantHost/PlantHost/HumiditySensorController.cs
+++ Connected_Plant/PlantHost/src/PlantHost/PlantHost/HumiditySensorController.cs
@@ -1,10 +1,38 @@
-﻿using System;
+﻿using MSH = Microsoft.SPOT.Hardware;
+using SLH = SecretLabs.NETMF.Hardware;
+using SLHN = SecretLabs.NETMF.Hardware.Netduino;
+using System.Threading;
+
 namespace PlantHost
 {
     public class HumiditySensorController
     {
-        public HumiditySensorController()
+        protected SLH.AnalogInput _analogPort;
+        protected MSH.OutputPort _digitalPort;
+
+        public HumiditySensorController(MSH.Cpu.Pin analogPort, MSH.Cpu.Pin digitalPort)
         {
+            _analogPort = new SLH.AnalogInput(analogPort);
+            _digitalPort = new MSH.OutputPort(digitalPort, false);
+        }
+
+        public float Read()
+        {
+            int sample;
+            float humidity;
+
+            _digitalPort.Write(true);
+            Thread.Sleep(5);
+            sample = _analogPort.Read();
+            _digitalPort.Write(false);
+
+            humidity = 100 - Map(sample, 250, 1023, 0, 100);
+            return humidity;
+        }
+
+        protected float Map(float value, float fromLow, float fromHigh, float toLow, float toHigh)
+        {
+            return (((toHigh - toLow) * (value - fromLow)) / (fromHigh - fromLow)) - toLow;
         }
     }
 }
