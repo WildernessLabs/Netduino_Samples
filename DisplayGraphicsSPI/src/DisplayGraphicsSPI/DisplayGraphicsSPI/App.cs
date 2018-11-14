e7db5c434eea4fe16160519e54adcd9caa72b498
Jorge Ramirez <ramirez.jorgea@outlook.com>
2018-11-13 23:10:01 -0800

    [DisplayGraphicsSPI] Showing Shapes and Lines // Text

--- DisplayGraphicsSPI/src/DisplayGraphicsSPI/DisplayGraphicsSPI/App.cs
+++ DisplayGraphicsSPI/src/DisplayGraphicsSPI/DisplayGraphicsSPI/App.cs
@@ -8,6 +8,7 @@ namespace DisplayGraphicsSPI
 {
     public class App
     {
+        const int DRAW_TIME = 500;
         protected ILI9163 tftDisplay;
         static GraphicsLibrary display;
 
@@ -40,49 +41,59 @@ namespace DisplayGraphicsSPI
         {
             display.Clear(true);
 
-            display.DrawRectangle(3,  3, 26, 26, Color.Red, true);
-            display.DrawRectangle(32, 3, 20, 20, Color.Red, true);
-            display.DrawRectangle(55, 3, 14, 14, Color.Red, true);
-            display.DrawRectangle(72, 3, 8, 8, Color.Red, true);
-            display.DrawRectangle(83, 3, 2, 2, Color.Red, true);
-            //display.Show();
-            //Thread.Sleep(1000);
+            display.DrawRectangle(3,  3, 26, 26, Color.Red, true); 
+            display.DrawRectangle(37, 6, 20, 20, Color.Red, true);
+            display.DrawRectangle(65, 9, 14, 14, Color.Red, true);
+            display.DrawRectangle(87, 12, 8, 8, Color.Red, true);
+            display.DrawRectangle(103, 15, 2, 2, Color.Red, true);
+            display.Show();
+            Thread.Sleep(DRAW_TIME);
 
             display.DrawRectangle(3,  32, 26, 26, Color.Red);
-            display.DrawRectangle(32, 32, 20, 20, Color.Red);
-            display.DrawRectangle(55, 32, 14, 14, Color.Red);
-            display.DrawRectangle(72, 32, 8, 8, Color.Red);
-            display.DrawRectangle(83, 32, 2, 2, Color.Red);
-            //display.Show();
-
-            display.DrawCircle(16, 74, 13, Color.Red, true);
-            display.DrawCircle(45, 74, 10, Color.Red, true);
-            display.DrawCircle(68, 74, 7, Color.Red, true);
-            display.DrawCircle(85, 74, 4, Color.Red, true);
-            display.DrawCircle(96, 74, 1, Color.Red, true);
+            display.DrawRectangle(37, 35, 20, 20, Color.Red);
+            display.DrawRectangle(65, 38, 14, 14, Color.Red);
+            display.DrawRectangle(87, 41, 8, 8, Color.Red);
+            display.DrawRectangle(103, 44, 2, 2, Color.Red);
             display.Show();
+            Thread.Sleep(DRAW_TIME);
 
-            //display.Clear(true);
-            //display.DrawCircle(63, 31, 20, true);
-            //display.DrawCircle(63, 15, 5, true);
-            //display.Show();
-            //Thread.Sleep(1000);
+            display.DrawCircle(16, 73, 13, Color.Green, true);
+            display.DrawCircle(47, 73, 10, Color.Green, true);
+            display.DrawCircle(72, 73, 7, Color.Green, true);
+            display.DrawCircle(91, 73, 4, Color.Green, true);
+            display.DrawCircle(104, 73, 1, Color.Green, true);
+            display.Show();
+            Thread.Sleep(DRAW_TIME);
 
-            //display.Clear(true);
-            //display.DrawRectangle(20, 20, 60, 40);
-            //display.DrawRectangle(20, 10, 60, 30);
-            //display.Show();
-            //Thread.Sleep(1000);
+            display.DrawCircle(16, 103, 13, Color.Green);
+            display.DrawCircle(47, 103, 10, Color.Green);
+            display.DrawCircle(72, 103, 7, Color.Green);
+            display.DrawCircle(91, 103, 4, Color.Green);
+            display.DrawCircle(104, 103, 1, Color.Green);
+            display.Show();
+            Thread.Sleep(DRAW_TIME);
 
-            //display.Clear(true);
-            //display.DrawRectangle(30, 10, 50, 40);
-            //display.DrawRectangle(50, 10, 50, 20);
-            //display.Show();
-            //Thread.Sleep(1000);
+            for (int i = 0; i < 9; i++)
+                display.DrawHorizontalLine(3, 123 + (i * 4), 26, Color.Blue);
+
+            for (int i = 0; i < 7; i++)
+                display.DrawVerticalLine(37 + (i * 4), 123, 33, Color.Blue);
+
+            display.DrawLine(70, 131, 94, 147, Color.Blue);
+            display.DrawLine(70, 123, 94, 155, Color.Blue);
+            display.DrawLine(78, 123, 86, 155, Color.Blue);
+            display.DrawLine(86, 123, 78, 155, Color.Blue);
+            display.DrawLine(94, 123, 70, 155, Color.Blue);
+            display.DrawLine(94, 131, 70, 147, Color.Blue);
+            display.DrawLine(70, 139, 94, 139, Color.Blue);
+            display.Show();
+            Thread.Sleep(DRAW_TIME);
         }
 
         protected void ShowTextSample()
         {
+            display.Clear(true);
+
             display.CurrentFont = new Font8x12();
             display.DrawText(4, 4, "abcdefghijklm", Color.SkyBlue);
             display.DrawText(4, 18, "nopqrstuvwxyz", Color.SkyBlue);
@@ -94,10 +105,10 @@ namespace DisplayGraphicsSPI
             display.DrawText(4, 102, "NOPQRSTUVWXYZ", Color.SkyBlue);
 
             display.CurrentFont = new Font4x8();
-            display.DrawText(4, 116, "ABCDEFGHIJKLMNOPQRSTUVWXYZ", Color.White);
-            display.DrawText(4, 126, "abcdefghijklmnopqrstuvwxyz", Color.White);
-            display.DrawText(4, 136, "01234567890!@#$%^&*()_+-=", Color.White);
-            display.DrawText(4, 146, "\\|;:'\",<.>/?[]{}", Color.White);
+            display.DrawText(4, 116, "ABCDEFGHIJKLMNOPQRSTUVWXYZ", Color.SkyBlue);
+            display.DrawText(4, 126, "abcdefghijklmnopqrstuvwxyz", Color.SkyBlue);
+            display.DrawText(4, 136, "01234567890!@#$%^&*()_+-=", Color.SkyBlue);
+            display.DrawText(4, 146, "\\|;:'\",<.>/?[]{}", Color.SkyBlue);
 
             display.Show();
             Thread.Sleep(20000);
@@ -106,7 +117,7 @@ namespace DisplayGraphicsSPI
         public void Run()
         {
             ShowShapes();
-            //ShowTextSample();
+            ShowTextSample();
         }
     }
 }
